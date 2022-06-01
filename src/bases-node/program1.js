1b8f14a0135814c93a6c9acc424566c9d7beb1b9
Alejandro Vaquera Lopez <alejandrovaqueralopez608@gmail.com>
2022-05-31 17:37:23 -0700

    new

---
+++ src/bases-node/program1.js
@@ -0,0 +1,32 @@
+////////////////////////////
+let cont = 0;
+const multiply = (stNum,ndNum) => {
+	let result = 0;
+	for(cont = 0; cont < ndNum; cont++){
+		result += stNum;
+	}
+	console.log(`The result (multiply) is: ${result}`);
+}
+multiply(5,8);
+
+////////////////////////////
+let ndCont = 0;
+const duply = (string) => {
+	let result = [];
+	for(ndCont = 0; ndCont < string.length; ndCont++){
+		result.push(string[ndCont]);
+		result.push(string[ndCont]);
+	}
+	result = result.join("");
+	console.log(`The result (duply) is: ${result}`);
+}
+duply("Hola");
+
+////////////////////////////
+const duply2 = (string) => {
+	const newArray = Array.from(string);
+	let result = newArray.map((item)=>item+item)
+	result = result.join("");
+	console.log(`The result (duply2) is: ${result}`);
+}
+duply2("Hola");
